bb00f6e6ac0c0d468ee4fd3691f96ad3d3af21e2
PrincessD75 <dikechiprincess@gmail.com>
2022-12-08 16:47:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - main block
-* return: Always 0
+* Return: 0
 */
 int main(void)
 {
-	puts("\" programming is like develop a multilingual puzzle");
+	puts("\" programming is like building a multilingual puzzle");
 	return (0);)
 }
