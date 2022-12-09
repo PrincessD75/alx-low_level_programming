830e35d1e48114e224bfbbfd190b95b0db4714b5
PrincessD75 <dikechiprincess@gmail.com>
2022-12-09 04:37:32 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include <stblib.h>
+/**
+ * main - main block
+ * description: get a random number and print number
+ */
+int main(void)
+{
+	 int n;
+	 stand(time(0))
