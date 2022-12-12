684f68337f640e6b50a14c6a8493299c90f5164e
PrincessD75 <dikechiprincess@gmail.com>
2022-12-12 08:07:02 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print random number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
