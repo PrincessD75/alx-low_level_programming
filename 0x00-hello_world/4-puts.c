796cbc28f2f8bda2d44a1395868dfb4715b5e1bf
PrincessD75 <dikechiprincess@gmail.com>
2022-12-12 13:03:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,11 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: 0
 */
-
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
 }
 
