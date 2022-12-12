73d8f1eacd8f0e49caee2033e2d8d1a2bd3504a2
PrincessD75 <dikechiprincess@gmail.com>
2022-12-12 07:21:49 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
