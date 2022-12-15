a6e4cd63491ac5bca90706a2ffcde8aaa5ec635b
PrincessD75 <dikechiprincess@gmail.com>
2022-12-14 16:41:25 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (count = 0, count < 92, count++)
+	for (count = 0; count < 92; count++)
 	{
 
 		sum = fib1 + fib2;
