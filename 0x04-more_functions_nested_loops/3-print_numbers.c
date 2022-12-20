bbaffc8e8a95071d93a89a0c059c4f0c79cdd3a0
PrincessD75 <dikechiprincess@gmail.com>
2022-12-19 19:02:33 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,12 @@
 
 void print_numbers(void);
 {
+	int start = 0, end = 9
 
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
+	while (start <= end)
 	{
-		_putchar(c);
+		_putchar(start + '0');
+		start++;
 	}
 	_putchar('\n');
 }
