b91bbfae6225fd2422df5232b1e01dabf43f1c70
PrincessD75 <dikechiprincess@gmail.com>
2022-12-19 18:02:39 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
