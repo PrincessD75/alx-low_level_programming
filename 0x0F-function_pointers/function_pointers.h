ac201986fb3227531ac67dc8c43e7f0ce2f5d87c
PrincessD75 <dikechiprincess@gmail.com>
2023-01-18 15:34:10 -0800

    3-op_functions.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,6 +11,8 @@ void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+int (*get_op_func(char *s))(int, int);
+
 int int_index(int *array, int size, int (*cmp)(int));
 
 
